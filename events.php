dd771879dd16b3261a1061cecfd91169a78c7baf
BRIHI-HOUSSAME <brihihossam@gmail.com>
2024-03-06 00:11:23 +0100

    Fex the log-in and sign-in alert

--- events.php
+++ events.php
@@ -35,7 +35,6 @@
 
 
 
-      
   <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
@@ -120,52 +119,56 @@
       </button>
     </div>
 
-    <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
-  <symbol id="check-circle-fill" viewBox="0 0 16 16">
-    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-  </symbol>
-  <symbol id="info-fill" viewBox="0 0 16 16">
-    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-  </symbol>
-  <symbol id="exclamation-triangle-fill" viewBox="0 0 16 16">
-    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-  </symbol>
-</svg>
+
 
     <?php
     include 'connection.php';
-
-    // Inscription form
     if (isset($_POST['INS-DONE'])) {
-
       if (!verify_user($_POST['email'], $DB)) {
-
-
-        $insertQuery = "INSERT INTO utilisateur (nom,prenom,email,motPasse) VALUES (:name,:lastName,:email,:password)";
-        $stmInsertUser = $DB->prepare($insertQuery);
-        $stmInsertUser->bindParam(':name', $_POST['nom']);
-        $stmInsertUser->bindParam(':lastName', $_POST['prenom']);
-        $stmInsertUser->bindParam(':email', $_POST['email']);
-        $stmInsertUser->bindParam(':password', $_POST['password']);
-        $stmInsertUser->execute();
-        echo '
-        <div class="alert alert-success d-flex align-items-center" role="alert">
-          <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
-          <div>
-          Votre compte a été créé avec succès
+          $insertQuery = "INSERT INTO utilisateur (nom, prenom, email, motPasse) VALUES (:name, :lastName, :email, :password)";
+          $stmInsertUser = $DB->prepare($insertQuery);
+          $stmInsertUser->bindParam(':name', $_POST['nom']);
+          $stmInsertUser->bindParam(':lastName', $_POST['prenom']);
+          $stmInsertUser->bindParam(':email', $_POST['email']);
+          $stmInsertUser->bindParam(':password', $_POST['password']);
+          $stmInsertUser->execute();
+          echo '
+          <div class="fixed-top w-100 " style="margin-top: 10px;" >
+              <div class="alert alert-success d-flex align-items-center justify-content-between alert-dismissible fade show px-5" role="alert">
+                  <div class="d-flex align-items-center justify-content-center gap-3">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi flex-shrink-0 me-2" role="img" aria-label="Success:" viewBox="0 0 16 16">
+                          <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+                      </svg>
+                      Inscription réussie
+                  </div>
+                  <div>
+                      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                  </div>
+              </div>
           </div>
-        </div>';
-
-      } else
-      echo '
-      <div class="alert alert-danger d-flex align-items-center" role="alert">
-      <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-      <div>
-      Email existent   
-    </div>
-    </div>';
-      
-    }
+          ';
+      } else {
+          echo '
+          <div class="fixed-top w-100 " style="margin-top: 100px;">
+              <div class="alert alert-danger d-flex align-items-center justify-content-between alert-dismissible fade show px-5" role="alert">
+                  <div class="d-flex align-items-center justify-content-center gap-3">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+                          <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+                      </svg>
+                      Email existant
+                  </div>
+                  <div>
+                      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                  </div>
+              </div>
+          </div>
+          ';
+      }
+  }
+  
+  
+  
+  
 
     // Initialize variables
     $startDate = "";
@@ -267,19 +270,7 @@
     }
     echo "</section>";
     ?>
-<div class="container margin-top">
-            <div class="alert alert-danger d-flex align-items-center justify-content-between alert-dismissible fade show px-5" role="alert">
-                <div class="d-flex align-items-center justify-content-center gap-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
-                        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
-                    </svg>
-      Email existent   
-                </div>
-                <div>
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            </div>
-        </div>
+
 
     <section class="features">
       <div class="feature">
